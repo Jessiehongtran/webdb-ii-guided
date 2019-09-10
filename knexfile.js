67eceb1ca7727043a972010ccbff24917bb2ba8c
Your Name <you@example.com>
2019-09-10 10:18:20 -0700

    removed production out of knexFile

--- knexfile.js
+++ knexfile.js
@@ -9,24 +9,4 @@ module.exports = {
     },
     useNullAsDefault: true
   }
-  
-
-  ,
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
